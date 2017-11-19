860ed25917cef4431c480befad5b96670108ddb3
Dmitrii.Sulimchuk <dserge.spb@gmail.com>
2017-11-19 18:25:57 +0300

    +vaadin for visualization

--- twitter-streaming-grabber/src/main/kotlin/com/example/twittergrabber/TwitterGrabberApplication.kt
+++ twitter-streaming-grabber/src/main/kotlin/com/example/twittergrabber/TwitterGrabberApplication.kt
@@ -2,12 +2,16 @@ package com.example.twittergrabber
 
 import com.example.core.domain.*
 import com.example.core.services.PersistentMessageQueue
+import com.vaadin.spring.annotation.EnableVaadin
 import mu.KLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint
+import org.springframework.data.mongodb.core.geo.GeoJsonPolygon
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import twitter4j.FilterQuery
+import twitter4j.GeoLocation
 import twitter4j.Status
 import twitter4j.TwitterStreamFactory
 import javax.annotation.PostConstruct
@@ -17,6 +21,8 @@ import javax.annotation.PostConstruct
         "com.example.twittergrabber"
 ))
 @EnableMongoRepositories(basePackages = arrayOf("com.example.core.repositories"))
+@EnableVaadin
+
 class TwitterGrabberApplication {
 
     private companion object : KLogging()
@@ -41,6 +47,7 @@ class TwitterGrabberApplication {
         twitterStream.onException { ex -> logger.error(ex) { } }
                 .onStatus { status -> persistentMessageQueue.offer(convertToMessage(status)) }
                 .filter(filterQuery)
+
     }
 
     private fun convertToMessage(status: Status) = Message(
@@ -56,13 +63,37 @@ class TwitterGrabberApplication {
                             gender = GenderEnum.UNDEFINED
                     )
             ),
-            location = Location(
-                    country = status.place.country,
-                    locationType = status.place.placeType,
-                    locationName = status.place.fullName
-            )
+            location = buildLocation(status)
     )
 
+    private fun buildCoordinates(geoLocation: GeoLocation?): GeoJsonPoint? {
+        return geoLocation?.toGeoJsonPoint()
+    }
+
+    private fun buildLocation(status: Status): Location {
+        return when (status.place.boundingBoxType) {
+            "Polygon" -> {
+                val points = status.place.boundingBoxCoordinates[0].map { it.toGeoJsonPoint() }
+                //first and last position must be the same
+                val boundingBox = GeoJsonPolygon(points + points.first())
+                Location(country = status.place.country,
+                        locationType = status.place.placeType,
+                        locationName = status.place.fullName,
+                        boundingBox = boundingBox,
+                        exactCoordinates = buildCoordinates(status.geoLocation)
+                )
+            }
+            else -> {
+                logger.error { "unknown location type in $status" }
+                throw RuntimeException("unknown location type in ${status.place}")
+            }
+        }
+    }
+
+    private fun GeoLocation.toGeoJsonPoint(): GeoJsonPoint {
+        // GeoJSON specifies a longitude then a latitude,
+        return GeoJsonPoint(this.longitude, this.latitude)
+    }
 }
 
 fun main(args: Array<String>) {
